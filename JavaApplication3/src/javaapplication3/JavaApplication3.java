22184739831b63ff86105023afeecf5eab8978ca
Valentin Stieger <stiegervalentin123@gmail.com>
2024-03-19 18:40:26 -0300

    si
    si

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -16,6 +16,9 @@ public class JavaApplication3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        System.out.println("In computer science, the Boolean (sometimes shortened to Bool) is a data type that has one of two possible values (usually denoted true and false) which is intended to represent the two truth values of logic and Boolean algebra. It is named after George Boole, who first defined an algebraic system of logic in the mid 19th century. The Boolean data type is primarily associated with conditional statements, which allow different actions by changing control flow depending on whether a programmer-specified Boolean condition evaluates to true or false. It is a special case of a more general logical data type—logic does not always need to be Boolean (see probabilistic logic).");
     }
     
 }
