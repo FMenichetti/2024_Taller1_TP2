1658470325690e7b91df7fa6c9392edb3dd13290
Edu Lopez Code <edulopez.dg@gmail.com>
2024-03-20 17:55:58 -0300

    Se agrego la clase prueba 1
    Dale con todo

---
+++ 2024_EDA_TP2/src/Prueba_1.java
@@ -0,0 +1,17 @@
+
+public class Prueba_1 {
+   //Atributos
+    private int num;
+    
+    
+    public int getNum() {
+        return num;
+    }    
+    public void setNum(int num) {
+        this.num = num;
+        System.out.println("Este es el setNum");
+    }
+    
+    
+    
+}
